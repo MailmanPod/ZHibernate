4e0184460634f32eeac0c506f924f1693f2eb62b
Damian Bruera <brueradamian@gmail.com>
2015-01-05 18:09:15 -0300

    Added command options

--- ZHibernate/src/main/java/org/polux/start/HibernateLauncher.java
+++ ZHibernate/src/main/java/org/polux/start/HibernateLauncher.java
@@ -1,13 +1,50 @@
 package org.polux.start;
 
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.polux.utility.UtilityLauncher;
 
 public class HibernateLauncher {
 
+	private static ArrayList<String> commandOptions = new ArrayList<String>(4);
+	private static Logger logger = LogManager.getLogger();
+
+	private static void fillOptions() {
+		commandOptions.add("-a");
+		commandOptions.add("-u");
+		commandOptions.add("-d");
+		commandOptions.add("-l");
+	}
+
+	private static boolean findOptions(String option) {
+
+		boolean flag = false;
+
+		for (String op : commandOptions) {
+			if (op.equalsIgnoreCase(option)) {
+				flag = true;
+				break;
+			}
+		}
+
+		return flag;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		UtilityLauncher launcher = new UtilityLauncher();
-		launcher.entryPoint(args);
+		String option = args[0];
+
+		fillOptions();
+
+		if (findOptions(option)) {
+			UtilityLauncher launcher = new UtilityLauncher();
+			launcher.entryPoint(args);
+		} else {
+			logger.warn("Incorrect Option");
+			logger.warn("Usage: " + commandOptions.toString().toString());
+		}
 	}
 }
--- ZHibernate/src/main/java/org/polux/utility/UtilityLauncher.java
+++ ZHibernate/src/main/java/org/polux/utility/UtilityLauncher.java
@@ -34,12 +34,33 @@ public final class UtilityLauncher {
 				.applySettings(config.getProperties());
 		factory = config.buildSessionFactory(builder.build());
 
-		int option = Integer.parseInt(args[0]);
+		// int option = Integer.parseInt(args[0]);
+
+		char option = new String(args[0]).charAt(1);
 
 		switch (option) {
-		case 1:
-			logger.info("Entry in option 1 - addItem");
+		case 'a':
+			logger.info("Entry in option - addItem");
 			addItem(args);
+			break;
+
+		case 'u':
+			logger.info("Entry option - updateItem");
+			break;
+
+		case 'd':
+			logger.warn("Entry option - deleteItem");
+			break;
+
+		case 'l':
+			logger.info("Entry option - listItems");
+			break;
+
+		default:
+			logger.error("Incorrect option");
+			factory.close();
+			logger.error("Exiting application");
+			System.exit(1);
 			break;
 		}
 	}
